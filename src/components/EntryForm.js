185395fd5ee2211b8a2d42bb66ad9a537586fd97
James <jamesfsafford@gmail.com>
2020-09-23 19:21:03 +0100

    new search box and button

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function EntryForm() {
     return (
-      <div>Hello</div>
-      
+      <div  class="topnav">
+      <a>Ingredient One</a>
+      <input type="text" placeholder="Search..."></input>
+      <a>Ingredient Two</a>
+      <input type="text" placeholder="Search..."></input>
+      <button class="favorite styled"
+        type="button">
+      Search</button> 
+      </div>
     );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,6 @@ function Header() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     );
 }
